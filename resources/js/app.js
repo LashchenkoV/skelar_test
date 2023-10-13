a03a2d43d8eaf31207628c9f22218b5397267f74
Viktor Lashchenko <v.laschenko@polytech.software>
2023-10-13 17:35:32 +0300

    install vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,8 +15,11 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        const vuetify = createVuetify({ components, directives });
+
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
